f9550c5691a3b8a1759280cfcc7aa3df0b162432
Zach <zjhuntingtonmeath@gmail.com>
2016-02-11 21:15:31 -0500

    Changed repo setup to work with my own Katello fork.

--- repo-setup.sh
+++ repo-setup.sh
@@ -2,10 +2,10 @@
 cd ~/katello
 git remote add upstream git@github.com:Katello/katello.git
 git fetch upstream
-git remote add fork git@github.com:johnpmitsch/katello.git
+git remote add fork git@github.com:zjhuntin/katello.git
 git fetch fork
 cp ~/config_settings/pre-push ~/katello/.git/hooks
 cp ~/config_settings/pre-push ~/foreman/.git/hooks
 cp ~/config_settings/ktest ~/foreman
-cp ~/config_settings/ktest-live ~/foreman 
+cp ~/config_settings/ktest-live ~/foreman
 echo "ktest*" >> ~/foreman/.git/info/exclude
