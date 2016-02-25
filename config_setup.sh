daa04600d5f7c8a20dc238a65efb19aeb7a6e872
Zach Huntington-Meath <zhunting@redhat.com>
2016-02-25 08:59:28 -0500

    Will copy .zshrc instead of .bashrc in config_setup.sh.

--- config_setup.sh
+++ config_setup.sh
@@ -2,7 +2,7 @@
 echo "setting up dev environment"
 echo "copying files"
 cp .vimrc_copy ~/.vimrc
-cp .bashrc_copy ~/.bashrc
+cp .zshrc_copy ~/.zshrc
 cp .pryrc_copy ~/.pryrc
 export PS1="\[\e[1;$(shuf -i31-36 -n1)m\][\u@\h \W\$(git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/{\1}/')]\$\[\e[0m\] "
 cp .gitconfig_copy ~/.gitconfig
@@ -14,4 +14,3 @@ echo 'gem "pry"' >> ~/foreman/bundler.d/katello.local.rb
 echo 'gem "pry-byebug"' >> ~/foreman/bundler.d/katello.local.rb
 cd ~/foreman
 bundle install
-
